9943c0769385cd363e38f0fbbaabddec6c6028bb
ryo ishida <r.ishida.cingroup2012@gmail.com>
2025-08-25 18:40:31 +0900

    vsriables 訂正版2

--- src/basicSyntax/Variables.java
+++ src/basicSyntax/Variables.java
@@ -5,8 +5,11 @@ public class Variables {
 	public static void main(String[] args) {
 	int num1 = 7;
 	double num2 = 0.5;
-	System.out.println("私の好きな整数は" + num1 + "です。");
-	System.out.println("私の好きな少数は" + num2 + "です。");
+	
+	String a = "私の好きな整数は";
+	String b = "です。";
+	System.out.println(a + num1 + b);
+	System.out.println(a + num2 + b);
 
 	}
 
