5839b260c3a9845b824bf9a6d92aba0424e6164a
ryo ishida <r.ishida.cingroup2012@gmail.com>
2025-08-25 18:30:48 +0900

    varables 訂正版

--- src/basicSyntax/Variables.java
+++ src/basicSyntax/Variables.java
@@ -3,10 +3,10 @@ package basicSyntax;
 public class Variables {
 
 	public static void main(String[] args) {
-	int number = 7;
-	double decimal = 0.5;
-	System.out.println("私の好きな整数は" + number + "です。");
-	System.out.println("私の好きな少数は" + decimal + "です。");
+	int num1 = 7;
+	double num2 = 0.5;
+	System.out.println("私の好きな整数は" + num1 + "です。");
+	System.out.println("私の好きな少数は" + num2 + "です。");
 
 	}
 
