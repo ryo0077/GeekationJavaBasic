c17e6a331de94e589c79f080832f4fc867611526
ryo ishida <r.ishida.cingroup2012@gmail.com>
2025-09-02 13:43:35 +0900

    method-practice

---
+++ src/basicSyntax/MethodPractice.java
@@ -0,0 +1,38 @@
+package basicSyntax;
+
+//public class MethodPractice {
+//
+//	public static void main(String[] args) {
+//		// TODO 自動生成されたメソッド・スタブ
+//
+//	}
+//
+//}
+public class MethodPractice {
+	 
+	
+		 public static int add(int c, int d) {
+			 return c + d;
+		 }
+		 public static int subtract(int c, int d) {
+			 return c - d;
+		 }
+		 public static int multiply(int c, int d) {
+			 return c * d;
+		 }
+		 public static int divide(int c, int d) {
+			 return c / d;
+		 }
+		 public static void main(String[] args) {
+			  int a = 10;
+			  int b = 5;
+		
+	     String x = "計算結果は";
+	     String y = "です。";
+	     
+	     System.out.println(x + add(a, b) + y);
+	     System.out.println(x + subtract(a, b) + y);
+	     System.out.println(x + multiply(a, b) + y);
+	     System.out.println(x + divide(a, b) + y);
+	}
+}
